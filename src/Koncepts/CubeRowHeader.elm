a41a899486c367b1d5e6eefee6e56e35e4dd0ea1
Daniel Ljunghill <danielljunghill@hotmail.com>
2021-04-26 22:06:16 +0200

    save work on cube  as tree

--- src/Koncepts/CubeRowHeader.elm
+++ src/Koncepts/CubeRowHeader.elm
@@ -100,5 +100,97 @@ createIndentedHeader maybeSelection dimensionalKoncepts =
 
 
 
+-- Generate tree for koncepts TREE
+-- 
 
-     
+
+rowSpanTree: DimensionalKoncept -> RowSpan
+rowSpanTree  =
+    let 
+        recRowSpan koncept =
+            case koncept of
+                DimensionalAbstract (_, childKoncepts) ->
+                    case childKoncepts of
+                        [] -> 1
+                        _ ->
+                            let 
+                                recSpan koncepts =
+                                    case koncepts of
+                                        [] -> 0
+                                        head :: tail ->
+                                            (recRowSpan head) + (recSpan tail)
+                            in    
+                                recSpan childKoncepts
+                DimensionalValue _ -> 1
+    in
+        recRowSpan >> RowSpan
+
+colSpanTree  =
+    let 
+        recCalcForOne stateOne koncept =      
+            let 
+                newStateOne = stateOne + 1
+            in 
+                case koncept of
+                    DimensionalAbstract (_,childKoncepts) ->
+                        let 
+                            recCalcForMany stateMany koncepts =
+                                case koncepts of
+                                    [] -> [ stateMany ]
+                                    head :: tail ->  (recCalcForOne stateMany head) ++ recCalcForMany stateMany tail     
+                        in           
+                            recCalcForMany newStateOne childKoncepts
+                    DimensionalValue _ -> [ newStateOne ] 
+    in
+        
+        recCalcForOne 0
+        >> Lists.maxInt
+        >> Maybe.withDefault 1
+        >> ColumnSpan
+
+cubeRowHeadersTree: List DimensionalKoncept -> List CubeRowHeader
+cubeRowHeadersTree dimensionalKontexts =
+    let 
+        maxColSpanForTree =
+            dimensionalKontexts
+            |> List.map colSpanTree
+            |> Lists.maxBy intColumnSpan
+   
+        recCubeHeaders: ColumnSpan -> Area -> List DimensionalKoncept -> List CubeHeader
+        recCubeHeaders maxColSpan arean kontexts  = 
+            case kontexts of
+                [] -> []
+                head :: tail ->
+                    let 
+                        rowSpan = rowSpanTree head
+                        recCubeHeadersInner: ColumnSpan -> Area -> DimensionalKoncept -> List CubeHeader
+                        recCubeHeadersInner maxColSpanForKoncept area childKoncepts =
+                            let 
+                                newMaxColSpan = decColSpan maxColSpanForKoncept 
+                            in
+                                case childKoncepts of
+                                    DimensionalAbstract (ak, koncepts) ->
+                                        case koncepts of
+                                            [] -> [{ 
+                                                            area = { area | columnSpan = maxColSpanForKoncept }
+                                                        ,   name = abstractKonceptNameToString ak.name 
+                                                    }]
+                                            child :: children ->                       
+                                                [{ 
+                                                        area =    { area | rowSpan = rowSpan }
+                                                        ,   name = abstractKonceptNameToString ak.name 
+                                                    }]
+                                                |> List.append (recCubeHeadersInner newMaxColSpan ({ area | column = incColumn area.column }) child)
+                                                |> List.append (recCubeHeaders newMaxColSpan ({ area | column = incColumn area.column , row = incRow area.row }) children)
+
+                                    DimensionalValue vk ->
+                                        [{ 
+                                                area =  { area | columnSpan = maxColSpanForKoncept }
+                                            ,   name = valueKonceptNameToString vk.name 
+                                        }]
+                    in
+                        recCubeHeadersInner maxColSpan arean head 
+                        |> List.append (recCubeHeaders maxColSpan ({arean | row = arean.row |> addRowSpanToRow rowSpan}) tail)
+    in
+        dimensionalKontexts
+        |> recCubeHeaders maxColSpanForTree areaOne      
